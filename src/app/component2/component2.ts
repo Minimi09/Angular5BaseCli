f3b9afdc23b3ad8a7218b03d53dfc183ce000225
Minimi09 <pereyraminely@gmail.com>
2018-04-20 12:18:30 -0500

    CheckfSmartGit

--- src/app/component2/component2.ts
+++ src/app/component2/component2.ts
@@ -21,7 +21,6 @@ export class LoginPassword implements OnInit, OnDestroy {
 
     loginUser( )
     {
-     
         this._loginService.checkLogin(this.user,this.password) ? this._router.navigate(['']) : alert('Error User/Password Invalidos');
     }
 
